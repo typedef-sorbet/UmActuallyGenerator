e47f9267d518d265e5842de5f9a129bf61f113c7
Spencer Warneke <warnespe001@gmail.com>
2022-11-19 15:40:17 -0800

    README, comments, bugfixing

---
+++ README.md
@@ -0,0 +1,30 @@
+## Overview
+
+UmActuallyGenerator allows the creation of custom "Um, Actually"-eqsue question cards from JSON data.
+
+
+## Usage
+
+JSON data can be provided either on the command-line or via a file:
+
+```bash
+    # JSON via Command Line:
+    python3 main.py --json '{...}'
+
+    # JSON from file:
+    python3 main.py data.json
+```
+
+Question card data can also be collected via stdin if no arguments are given.
+
+## JSON Schema
+
+```json
+{
+    "title": "Title of the property/subject in question (e.g. God of War, Akira, etc.)",
+    "category": "Genre of the property/subject in question (e.g. Games, TV, etc.)",
+    "statement": "The incorrect statement to be read. The incorrect part to be highlighted should be placed within square brackets [like this]",
+    "correction": "The correction to the above statement",
+    "details": "Additional details about the correction"
+}
+```
--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-from typing import List
-import imgkit       # For rendering HTML to an image
-import json         # ...for JSON
-import argparse     # For parsing arguments
-import random       # For filenames
+from typing import List     # For knowing whether an object is a list
+import imgkit               # For rendering HTML to an image
+import json                 # ...for JSON
+import argparse             # For parsing arguments
+import random               # For filenames
 
-from PIL import Image, ImageChops
-from jinja2 import Environment, FileSystemLoader, select_autoescape       # For templating HTML
-from os.path import exists
+from PIL import Image, ImageChops                                       # For image processing
+from jinja2 import Environment, FileSystemLoader, select_autoescape     # For templating HTML
+from os.path import exists                                              # For filesystem stuff
+from os import mkdir                                                    # ^^^^^^^^^^^^^^^^^^^
 
 jinja_env = Environment(
     loader = FileSystemLoader("templates/"),
     autoescape = select_autoescape()
 )
 
+# Don't crop vertically; trim() needs to be able to grab whitespace from (0,0)
 imgkit_opts = {
     "crop-x": 10,
     "crop-w": 628
 }
 
+# Trims bordering pixels from the image; border color inferred from top-left pixel
+# Props to fraxel for the basics of this function, and Matt Pitkin for RGBA workarounds
+# https://stackoverflow.com/questions/10615901/trim-whitespace-using-pil
 def trim(im):
     bg = Image.new("RGB", im.size, im.getpixel((0,0)))
     diff = ImageChops.difference(im.convert("RGB"), bg)
@@ -28,6 +33,7 @@ def trim(im):
     else:
         return im
 
+# Create dict from JSON string
 def from_json(json_str):
     json_data = json.loads(json_str)
 
@@ -36,16 +42,17 @@ def from_json(json_str):
 
     return json_data, None
 
+# Create List[Dict] from user input
 def from_stdin():
     return [{
         "title": input("Title of the property in question: "),
         "category": input("Category of the property in question (e..g. 'Horror', 'Anime', etc.): "),
-        # Lord forgive me.
         "statement": input("Statement (Place the incorrect part of the statement in square brackets, [like this])"),
         "correction": input("Correction: "),
         "details": input("Additional details: ")
     }], None
 
+# Create dict from JSON file
 def from_file(filename):
     if exists(filename):
         with open(filename, "r") as infile:
@@ -71,6 +78,7 @@ def main():
     else:
         template_data, reason = from_stdin()
 
+    # Default to using a list of dicts -- if we only got one element, chuck it into a list anyways.
     if not template_data:
         print(reason)
         return
@@ -79,10 +87,15 @@ def main():
 
     template = jinja_env.get_template("card.html")
 
+    if not exists("out/"):
+        mkdir("out/")
+
     for dikt in template_data:
+        # Render HTML viewport, with some extra whitespace
         rand = random.randint(10000, 99999)
         imgkit.from_string(template.render(data=dikt), f"out/{dikt['title'].replace(' ', '_')}_{rand}.png", options=imgkit_opts)
 
+        # Trim aforementioned whitespace so the image doesn't look terrible
         im = Image.open(f"out/{dikt['title'].replace(' ', '_')}_{rand}.png")
         im = trim(im)
         im.save(f"out/{dikt['title'].replace(' ', '_')}_{rand}.png")
